5c04e6ddffe7ae2f914c01ee46a904839c70915c
Daniel López Berrocal <97476157+DaniLoBerr@users.noreply.github.com>
2024-11-08 13:14:21 +0100

    Begin construction of the class "Reservation"
    Set up the structure of the class "Reservation" and code its
    __init__ method.

--- reservation.py
+++ reservation.py
@@ -18,11 +18,81 @@
     - i/o
     - pytest
 """
-class Reservation():
-    # date
-    # time
-    # people
-    ...
+class Reservation:
+    """
+    A class representing a reservation.
+
+    Attributtes:
+        name (str): The name of the person making the reservation.
+        date (...): The date of the reservation.
+        time (...): The time of the reservation.
+        people (int): The number of people who will attend
+    """
+
+    def __init__(self, name, date, time, people):
+        """
+        Initializes a Reservation object.
+
+        Parameters:
+            name (str): The name of the person making the reservation.
+            date (...): The date of the reservation.
+            time (...): The time of the reservation.
+            people (int): The number of people who will attend
+        """
+        self.name = name
+        self.date = date
+        self.time = time
+        self.people = people
+    
+    def __string__(self):
+        """
+        ...
+
+        Returns:
+            ...
+        """
+        ...
+    
+    @property
+    def name(self):
+        ...
+    
+    @name.setter
+    def name(self):
+        ...
+    
+    @property
+    def date(self):
+        ...
+    
+    @date.setter
+    def date(self):
+        ...
+    
+    @property
+    def time(self):
+        ...
+    
+    @time.setter
+    def time(self):
+        ...
+    
+    @property
+    def people(self):
+        ...
+    
+    @people.setter
+    def people(self):
+        ...
+    
+    def delete(self):
+        ...
+    
+    def update(self):
+        ...
+    
+    def confirmation(self):
+        ...
 
 
 def main():
