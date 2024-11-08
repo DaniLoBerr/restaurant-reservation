bea1dcccb4dcc1a1939d780f61ab270ca5f4fe3c
Daniel López Berrocal <97476157+DaniLoBerr@users.noreply.github.com>
2024-11-08 12:17:44 +0100

    Set up project structure

---
+++ reservation.py
@@ -0,0 +1,33 @@
+# Getting Started
+"""
+- What will your software do? What features will it have? How will it be executed?
+    - Aplicación de terminal
+    - Mensaje de bienvenida
+    - Preguntar por fecha de la reserva y personas (incluir el formato, incluir posibilidad de cancelación)
+        - Si está escrito en otro formato, repreguntar
+        - Si está completo ese día y/o ese hora, repreguntar
+    - Mensaje de:
+        - Cancelación si decide cancelar.
+        - Reserva completada si se ha completado.
+            - Guardar en un archivo json los datos de todas las reservas
+            - Exportar un pdf con la confirmación, la fecha y la hora de la reserva.
+- What new skills will you need to acquire? What topics will you need to research?
+    - Librería date
+    - Clases
+    - try-catch
+    - i/o
+    - pytest
+"""
+class Reservation():
+    # date
+    # time
+    # people
+    ...
+
+
+def main():
+    ...
+
+
+if __name__ == "__main__":
+    main()
